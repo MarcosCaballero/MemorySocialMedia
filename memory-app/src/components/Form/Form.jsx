060a66f6d327787560cd975047f21a8dccfae6ad
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-01-11 01:15:55 -0300

    Formulario terminado
    Se añadio el formulario y se añadio su action y payload de creacion a la api/index, además se agregó su action Create

--- memory-app/src/components/Form/Form.jsx
+++ memory-app/src/components/Form/Form.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
+
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
+
+import useStyles from "./styles";
 
 const Form = () => {
-  return <h1>Form</h1>;
+  const dispatch = useDispatch();
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const classes = useStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData));
+  };
+
+  const handlerChange = (e) => {
+    setPostData({
+      ...postData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handlerClear = () => {
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="creator"
+          fullWidth
+          value={postData.creator}
+          onChange={handlerChange}
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="title"
+          fullWidth
+          value={postData.title}
+          onChange={handlerChange}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="message"
+          fullWidth
+          value={postData.message}
+          onChange={handlerChange}
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="tags"
+          fullWidth
+          value={postData.tags}
+          onChange={handlerChange}
+        />
+        <div className={classes.fileinput}>
+          <FileBase
+            name="selectedFile"
+            type="file"
+            multiple={false}
+            value={postData.selectedFile}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={handlerClear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
--- memory-app/src/components/Form/styles.jsx
+++ memory-app/src/components/Form/styles.jsx
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   root: {
-    "& .MuiTextField-foot": {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
     },
   },
