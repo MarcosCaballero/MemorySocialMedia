acd2a4e36e3fdfc1cce71e27bcb1c8efaa6c5c25
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-01-18 23:38:17 -0300

    update y delete post api
    Se añade la la ruta en el front para poder  hacer una peticion update y delete.

--- memory-app/src/api/index.js
+++ memory-app/src/api/index.js
@@ -4,5 +4,6 @@ const url = "http://localhost:5000/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (updatePost) =>
-  axios.patch(`${url}/update`, updatePost);
+export const updatePost = (id, updatePost) =>
+  axios.patch(`${url}/${id}`, updatePost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
