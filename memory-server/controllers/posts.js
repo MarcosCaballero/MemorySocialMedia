cd32986ad949afe9359a8d134ffc85a2e1017c72
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-01-18 23:47:32 -0300

    update y delete backend
    Se añaden las rutas de update y delete en la api de la aplicación, además se añaden sus respectivos controladores.

--- memory-server/controllers/posts.js
+++ memory-server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -21,5 +22,58 @@ export const createPost = async (req, res) => {
 };
 
 export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
   const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res
+      .status(404)
+      .json({ message: "No se encontro un post con ese id" });
+  } else {
+    const updatedPost = await PostMessage.findByIdAndUpdate(
+      _id,
+      { ...post, _id },
+      {
+        new: true,
+      }
+    );
+    return res.json(updatedPost);
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res
+      .status(404)
+      .json({ message: "No se encontro un post con ese id" });
+  } else {
+    await PostMessage.findByIdAndRemove(_id);
+    res.json({ message: "Post deleted successfully" });
+    console.log("DELETE!");
+  }
+};
+
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res
+      .status(404)
+      .json({ message: "No se econtro ningun post con ese id" });
+  } else {
+    const post = await postMessage.findById(_id);
+
+    const updatedPOSt = await PostMessage.findByIdAndUpdate(
+      id,
+      {
+        likeCount: post.likeCount + 1,
+      },
+      {
+        new: true,
+      }
+    );
+
+    return res.json(updatedPost);
+  }
 };
